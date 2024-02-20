b42c5634bda87c052c3e1cca8426bcd7f1773037
parthpandyappp <parthpandyappp@gmail.com>
2024-02-20 23:26:54 +0530

    feat: get user data endpoint

--- htracker-server/controllers/Profile.js
+++ htracker-server/controllers/Profile.js
@@ -32,4 +32,20 @@ router.put("/:userId/history", async (req, res) => {
   }
 });
 
+router.get("/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
