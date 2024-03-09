2289c3287fa350715ba23ea1efce3eb3bdffac37
parthpandyappp <parthpandyappp@gmail.com>
2024-03-09 16:50:12 +0530

    endpoint: get user data by Id

--- htracker-server/controllers/Profile.js
+++ htracker-server/controllers/Profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.put("/:userId/listofhabits", async (req, res) => {
+  console.log(req.params);
+
   try {
     const userId = req.params.userId;
     const { listOfHabits } = req.body;
@@ -18,6 +20,7 @@ router.put("/:userId/listofhabits", async (req, res) => {
 });
 
 router.put("/:userId/history", async (req, res) => {
+  console.log(req.params);
   try {
     const userId = req.params.userId;
     const { activityHistory } = req.body;
@@ -26,6 +29,17 @@ router.put("/:userId/history", async (req, res) => {
       { activityHistory },
       { new: true }
     );
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: "Failed to update listOfHabits" });
+  }
+});
+
+router.get("/:userId", async (req, res) => {
+  try {
+    console.log(req.params);
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
     res.json(user);
   } catch (error) {
     res.status(400).json({ error: "Failed to update listOfHabits" });
